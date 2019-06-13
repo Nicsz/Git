c761549e06f977e6a9e15bfdfc490bc87b6e7778
nicsz1 <1vlad1litvinov1@gmail.com>
2019-06-13 13:12:59 +0300

    title change

--- index.php
+++ index.php
@@ -5,16 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Branch 2</title>
 </head>
 <body>
     <h1>Hello world!</h1>
     <h2>Something</h2>
-<<<<<<< HEAD
     <p>Text</p>
-=======
     <p>....</p>
     aesdaefdsfads
->>>>>>> my-first
 </body>
 </html>
